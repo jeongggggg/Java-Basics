50ec6b1fd83bee36d8158c260c5338b8adcecff8
dejeong <ahjeong921@gmail.com>
2024-09-02 11:41:51 +0900

    참조 타입 변수, String 예외

---
+++ src/chap02/Sample.java
@@ -0,0 +1,12 @@
+package chap02;
+
+public class Sample {
+    public static void main(String[] args) {
+        // 참조 자료형 : new 예약어를 사용하여 변수를 초기화
+        // String 예외
+        String str = "실제값";
+        String str2 = new String("실제값");
+        String str3 = "";
+
+    }
+}
