b6baa201bf19397e53d07802d806e7dada674951
dejeong <ahjeong921@gmail.com>
2024-09-03 15:20:19 +0900

    비트 논리 연산자, 비트 쉬프트 연산자

--- src/chap03/BitOperator.java
+++ src/chap03/BitOperator.java
@@ -13,6 +13,9 @@ public class BitOperator {
 
         // 비트 쉬프트 연산자
         int value = 8;
+        int value2 = -8;
         System.out.println(value >>> 1);
+        System.out.println(value2 >> 2);
+        System.out.println(value2 >>> 1); // 2147483644
     }
 }
