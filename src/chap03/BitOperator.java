07f55084274975d80e27acd5fb6fdec095647996
dejeong <ahjeong921@gmail.com>
2024-09-03 14:23:30 +0900

    비트 논리 연산자

---
+++ src/chap03/BitOperator.java
@@ -0,0 +1,15 @@
+package chap03;
+
+public class BitOperator {
+    public static void main(String[] args) {
+        // 비트 논리 연산자
+        int x = 15;
+        int y = 30;
+
+        System.out.println(x & y);
+        System.out.println(x | y);
+        System.out.println(x ^ y);
+        System.out.println(~x);
+
+    }
+}
