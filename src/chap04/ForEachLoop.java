f188669de96a33543c2c7ade38a121b1c363897a
dejeong <ahjeong921@gmail.com>
2024-09-04 13:30:17 +0900

    for문

---
+++ src/chap04/ForEachLoop.java
@@ -0,0 +1,11 @@
+package chap04;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String[] numbers = {"one", "two", "three", "four"};
+
+        for (String number : numbers) { // 선언한 number 변수는 for안에서 사용할 수 있게 된다.
+            System.out.println(number);
+        }
+    }
+}
