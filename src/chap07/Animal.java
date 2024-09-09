92ddcaff9503e9918c7a4f825ff390c17e516eb5
dejeong <ahjeong921@gmail.com>
2024-09-09 10:36:55 +0900

    객체 생성 순서 확인

--- src/chap07/Animal.java
+++ src/chap07/Animal.java
@@ -2,6 +2,11 @@ package chap07;
 
 public class Animal {
     String name;
+    
+    // 객체 생성 순서 확인
+    Animal(){
+        System.out.println("Animal 객체 생성");
+    }
 
     void setName(String name){
         this.name = name;
--- src/chap07/Dog.java
+++ src/chap07/Dog.java
@@ -4,6 +4,13 @@ package chap07;
 // 메서드 오버로딩
 // 리턴타입 메서드명 파라미터 개수 모두 같은데 재정의 한다면 오버라이딩
 public class Dog extends Animal{
+    
+    // 객체 생성 순서 확인
+    Dog(String name){
+        // super(); 부모의 값에 아무런 값이 없는 생성자를 호출해서 빈 값의 super이 호출된다.
+        System.out.println("Dog 객체 생성" + name);
+    }
+    
     void sleep(int times){ // 메서드를 재정의 했지만 뒤에 오는 파라미터 타입이 다르기 때문에 오버로딩(시그니처가 달라짐)
         System.out.println(name + " Zzz... " + times + " hours");
     }
