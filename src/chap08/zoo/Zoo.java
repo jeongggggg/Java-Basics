cebbc322ca5985f4e69b5c89e66c0d28e3ae7f58
dejeong <ahjeong921@gmail.com>
2024-09-10 11:19:51 +0900

    인터페이스를 사용하지 않고 메서드 호출 예제

---
+++ src/chap08/zoo/Zoo.java
@@ -0,0 +1,18 @@
+package chap08.zoo;
+
+/**
+ *인터페이스를 사용하지 않고
+ * 사육사가 각 동물마다 먹이를 주는
+ * zookeeper.feed() 메서드 호출
+*/
+public class Zoo {
+    public static void main(String[] args) {
+        zookeeper zookepper = new zookeeper();
+
+        Lion lion = new Lion();
+        zookepper.feed(lion);
+
+        Tiger tiger = new Tiger();
+        zookepper.feed(tiger);
+    }
+}
---
+++ src/chap08/zoo/zookeeper.java
@@ -0,0 +1,11 @@
+package chap08.zoo;
+
+public class zookeeper {
+    void feed(Lion lion){
+        System.out.println("feed fish");
+    }
+
+    void feed(Tiger tiger){
+        System.out.println("feed meat");
+    }
+}
