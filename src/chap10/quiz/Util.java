56c132c19d62852161ff5c949cc8af7ebd2a00c4
dejeong <ahjeong921@gmail.com>
2024-09-19 17:12:26 +0900

    데일리 퀴즈 풀이

--- src/chap10/quiz/Util.java
+++ src/chap10/quiz/Util.java
@@ -1,4 +1,10 @@
 package chap10.quiz;
+/*
+    Util.getValue() 메소드는 첫 번째 매개값으로 Pair 타입과 하위 타입만 받고,
+    두 번째 매개값으로 키를 받습니다.
+    리턴값은 키값이 일치할 경우 Pair에 저장된 값을 리턴하고,
+    일치하지 않으면 null을 리턴하도록 getValue() 제네릭 메소드를 정의해보세요.
+ */
 
 public class Util {
     public static <K, V> V getValue(Pair<? extends K, V> pair, K key) {
