ff34df0c244e0bb98dcd059407a2e3d3d8ac653e
dejeong <ahjeong921@gmail.com>
2024-09-23 15:18:00 +0900

    Predicate 구현 객체

--- src/chap12/LambdaExample2.java
+++ src/chap12/LambdaExample2.java
@@ -1,9 +1,6 @@
 package chap12;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.IntSupplier;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class LambdaExample2 {
     public static void main(String[] args) {
@@ -26,5 +23,13 @@ public class LambdaExample2 {
         Function<String, Double> function2 = x -> Double.valueOf(x) * 1.2;
         Double resultFunction = function2.apply("56.7"); // 문자열을 Double 타입으로 반환하고 1.2를 곱해줌 -> Double 타입으로 출력
         System.out.println("String to Double : " + resultFunction);
+
+        // Predicate 구현 객체를 람다식으로 작성
+        // test 라는 가상 클래스 사용
+        Predicate<String> isEmpty = (x) -> x.length() == 0;
+        System.out.println("빈값 체크 : " + isEmpty.test(""));
+        System.out.println("빈값 체크 : " + isEmpty.test("12345678"));
+        Predicate<Integer> predicate = x -> x % 2 == 0;
+        System.out.println("짝수 체크 : " + predicate.test(19));
     }
 }
