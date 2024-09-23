90d0a676fbaa004dd812f017c693aee4c10f6dea
dejeong <ahjeong921@gmail.com>
2024-09-23 13:48:26 +0900

    람다식 예제 설명 추가

--- src/chap12/custom/MyFunctionalInterface4Impl.java
+++ src/chap12/custom/MyFunctionalInterface4Impl.java
@@ -1,5 +1,6 @@
 package chap12.custom;
 
+// 매개변수가 2개, 리턴타입이 있는 함수형 인터페이스 -> 실제 구현은 람다식으로 구현
 public class MyFunctionalInterface4Impl implements MyFunctionalInterface4{
     @Override
     public int method(int x, int y) {
