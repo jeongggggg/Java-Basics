3ddec390778a6337a26c95c38c5a0b570a8f5891
dejeong <ahjeong921@gmail.com>
2024-09-24 17:37:22 +0900

    생성자 참조

--- src/chap12/ConstructorReferencesExample.java
+++ src/chap12/ConstructorReferencesExample.java
@@ -5,11 +5,10 @@ import java.util.function.Function;
 
 public class ConstructorReferencesExample {
     public static void main(String[] args) {
-        Function<String, Member> function = (x) -> new Member(x);
-        // 생성자 참조
-        function = Member::new;
-         // 인풋으로 2개의 매개변수 값이 들어가니까 BiFunction 사용
-        BiFunction<String, String, Member> function2 = (x,y) -> new Member(x,y);
-        function2 = Member::new;
+        Function<String, Member> function = Member::new;
+        function.apply("string");
+
+        BiFunction<String, String, Member> function2 = Member::new;
+        function2.apply("string", "string2");
     }
 }
